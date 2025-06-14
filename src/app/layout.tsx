75da784e4cc38ef9b0a5ab0a20bcc3a6af565e7e
Yasir Khan <yasirkhan5162@gmail.com>
2025-06-14 18:12:28 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/contexts/AuthContext';
@@ -25,6 +26,20 @@ export default function RootLayout({
           {children}
           <Toaster />
         </AuthProvider>
+        
+        {/* Google Tag Manager (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FX1KCV8SMB"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FX1KCV8SMB');
+          `}
+        </Script>
       </body>
     </html>
   );
